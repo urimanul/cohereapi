0044bcb993ac047ae08a63d87f3f3eeef16916e3
urimanul <myopera9@gmail.com>
2024-11-10 17:11:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
-from langchain_cohere import CohereEmbeddings
+from langchain.embeddings import CohereEmbeddings
 
-# CohereEmbeddingsの初期化 embeddings = CohereEmbeddings( api_key="GqsxZlKmcBzSultkVOfKPf7kVhYkporXvivq9KHg", # Node.jsではprocess.env.COHERE_API_KEYがデフォルト batch_size=48, # 省略時のデフォルト値は48。最大値は96 model="embed-english-v3.0" )
+# CohereEmbeddingsの初期化
+embeddings = CohereEmbeddings(
+    api_key="GqsxZlKmcBzSultkVOfKPf7kVhYkporXvivq9KHg",  # Node.jsではprocess.env.COHERE_API_KEYがデフォルト
+    batch_size=48,  # 省略時のデフォルト値は48。最大値は96
+    model="embed-english-v3.0"
+)
 
 # 初期設定
 st.set_page_config(page_title="プロンプト生成", layout="centered")
