7a79329424f0f2ba0c501be2534a21d212d04c33
urimanul <myopera9@gmail.com>
2024-11-11 00:47:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,135 @@
-import streamlit as st
-from langchain.embeddings import CohereEmbeddings
+# pip install streamlit langchain lanchain-openai beautifulsoup4 python-dotenv chromadb
 
-# CohereEmbeddingsの初期化
-embeddings = CohereEmbeddings(
-    api_key="GqsxZlKmcBzSultkVOfKPf7kVhYkporXvivq9KHg",  # Node.jsではprocess.env.COHERE_API_KEYがデフォルト
-    batch_size=48,  # 省略時のデフォルト値は48。最大値は96
-    model="embed-english-v3.0"
+import streamlit as st
+import mysql.connector as mydb
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+# コネクションの作成
+conn = mydb.connect(
+    host='www.ryhintl.com',
+    port='36000',
+    user='smairuser',
+    password='smairuser',
+    database='smair'
 )
 
-# 初期設定
-st.set_page_config(page_title="プロンプト生成", layout="centered")
-st.write(embeddings)
+cur = conn.cursor()
+cur.execute("SELECT * FROM openai_payload")
+
+# 全てのデータを取得
+rows = cur.fetchall()
+    
+# Get api_key
+for (spoid, key_name, api_key) in rows:
+        OPENAI_API_KEY=f"{api_key}"
+        #api_key = f"{api_key}"
+        #print(api_key)
+    
+cur.close()
+conn.close()
+
+#load_dotenv()
+
+
+def get_vectorstore_from_url(url):
+    # get the text in document form
+    loader = WebBaseLoader(url)
+    document = loader.load()
+    
+    # split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter()
+    document_chunks = text_splitter.split_documents(document)
+    
+    # create a vectorstore from the chunks
+    #vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    vector_store = FAISS.from_documents(document_chunks, OpenAIEmbeddings())
+
+    return vector_store
+
+def get_context_retriever_chain(vector_store):
+    llm = ChatOpenAI(openai_api_key=f"{OPENAI_API_KEY}")
+    
+    retriever = vector_store.as_retriever()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+    
+    return retriever_chain
+    
+def get_conversational_rag_chain(retriever_chain): 
+    
+    llm = ChatOpenAI()
+    
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    
+    stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
+    
+    return create_retrieval_chain(retriever_chain, stuff_documents_chain)
+
+def get_response(user_input):
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_input
+    })
+    
+    return response['answer']
+
+# app config
+st.set_page_config(page_title="Chat with websites", page_icon="🤖")
+st.title("Chat with websites")
+
+# sidebar
+with st.sidebar:
+    st.header("設定")
+    website_url = st.text_input("ウェブサイト URL")
+
+if website_url is None or website_url == "":
+    st.info("ウェブサイトの URL を入力してください")
+
+else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="こんにちは、ボットです。どんな御用でしょうか？"),
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)    
+
+    # user input
+    user_query = st.chat_input("ここにメッセージを入力してください...")
+    if user_query is not None and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
+        
+       
+
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("人間"):
+                st.write(message.content)
